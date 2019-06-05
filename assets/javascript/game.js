af0b1e99b374035e3e319608056543a554962ea2
Red Tail <redtail814@gmail.com>
2019-06-04 23:59:32 -0400

    Updated js to add guesses to guesses div.  Adds wins when user guesses right and then resets to next round.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,50 +6,52 @@ var wins = 0;
 var losses = 0;
 var userGuess = document.onkeyup;
 
-// Create function to select computer's answer.
-function computerAnswer() {
-    const answer = letterList[Math.floor(Math.random() * letterList.length)];
+// Create object to select computer's answer.
+function compAnswer() {
+    answer = letterList[Math.floor(Math.random() * letterList.length)];
     return answer;
-};    
-console.log(computerAnswer());
+}
 
-
-// Create a function to move on to next round
+// Create a function to move on to next round.
 function nextRound() {
+    guesses = [];
+    guessesLeft = 10;
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    document.getElementById("guesses").innerHTML = guesses;
+    compAnswer();   //------nextRound function finishes with generating a new answer for user to guess.
+    //--- console.log(compAnswer()); //---Remove comments to see new answer.
 }
 
 
 // Capture user's guess and check against answer.
 document.onkeyup = function (keypress) {
-    var userGuess = keypress.key;
-
+    let userGuess = keypress.key;
+    
     // Create a function to display user's wrong guesses in guesses element.
     function displayGuesses() {
-        if (guessesLeft == 10) {
-            document.getElementById("guesses").innerHTML += userGuess;
-        } else {
-            document.getElementById("guesses").innerHTML += "  " + userGuess;
-        }    
+        document.getElementById("guesses").innerHTML += " " + userGuess;  
     }    
+
     // Only run the code if a letter is chosen from letterList
     for (var i = 0; i < letterList.length; i++) {
         if (userGuess === letterList[i]) {
             
             // If user's guess equals the answer, add 1 to wins and move to next round.
-            if (userGuess === computerAnswer()) {
+            if (userGuess === answer)  {
                 wins++;
                 document.getElementById("wins").innerHTML = wins;
-                console.log(wins);
+                alert("You guessed right!  Press ok to move on to the next round.");
                 nextRound();
 
                 // If user's guess is wrong, push guessed letter to guesses array and subtract 1 guess from guesses left.        
-            } else if (userGuess !== computerAnswer()) {
+            } else if (userGuess !== answer) {
                 guessesLeft--;
                 document.getElementById("guesses-left").innerHTML = guessesLeft;
                 displayGuesses();
 
                 // If user has no remaining guesses, add 1 to losses and move to next round.
             } else {
+                // needs to be fixed.  as of now, user has unlimited guesses and can never lose.....
                 losses++;
                 document.getElementById("losses").innerHTML = losses;
                 console.log(losses);
@@ -62,4 +64,5 @@ document.onkeyup = function (keypress) {
 
 
 
-
+compAnswer();
+console.log(compAnswer());
